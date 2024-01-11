98036dcb28b691161fd4f73b52c19b7a212ba562
Yey007 <yeeeeeeey007@gmail.com>
2024-01-11 14:06:15 +0300

    Syntax changes

--- programs/hello_world.asm
+++ programs/hello_world.asm
@@ -1,19 +1,19 @@
 start:
     ssj
     ssf
-    ldi x, 0x4
-    ldi y, 0x8
+    ldi x 0x4
+    ldi y 0x8
     brn write_char
-    ldi x, 0x6
-    ldi y, 0x9
+    ldi x 0x6
+    ldi y 0x9
     brn write_char
     rsj
 end:
     brn end
 write_char:
-    mov z, x
+    mov z x
     out 0
-    mov z, y
+    mov z y
     out 1
     sep 0    ; in the simulation delay doesn't really matter, but in real life we would want something here
     rsp 0
